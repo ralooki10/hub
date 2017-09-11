f1ddc28894d160a06bfe843d4d0cac7eda0f8fcd
Raluca Puichilita <raluca.puichilita@gmail.com>
2017-09-11 13:20:45 +0100

    Super bare index page with header

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <div>Yaaaaaaaay!</div>
+import Head from 'next/head'
+
+export default () =>
+    <div>
+        <Head>
+            <title>Raluca Puichilita</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <p>Super bare page I'm not sure what to do with</p>
+    </div>
