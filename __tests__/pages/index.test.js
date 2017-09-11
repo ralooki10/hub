6a346011eea4fb9d3137ae7c3300d2540aef9d93
Raluca Puichilita <raluca.puichilita@gmail.com>
2017-09-11 15:53:34 +0100

    Update test to something meaningful to check page gets rendered as expected

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -1,5 +1,11 @@
-describe('nothing really', () => {
-    it('should prove that tests can run', () => {
-        expect("2").toBeTruthy();
+import renderer from 'react-test-renderer'
+import Page from '../../pages/index.js'
+
+describe('homepage look and feel', () => {
+    it('should render correctly', () => {
+        const component = renderer.create(<Page />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
     })
 });
+
